0d23dab906bb3380cc8f0bdfc4846f0751616a3f
Boris Bügling <boris@icculus.org>
2012-01-14 02:13:29 +0100

    Initial Commit

---
+++ FlattrKit/VUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VUAppDelegate.h
+//  FlattrKit
+//
+//  Created by Boris Bügling on 14.01.12.
+//  Copyright (c) 2012 Crocodil.us. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlattrKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlattrKit
+//
+//  Created by Boris Bügling on 14.01.12.
+//  Copyright (c) 2012 Crocodil.us. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VUAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VUAppDelegate class]));
+    }
+}
